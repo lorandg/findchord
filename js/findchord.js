e7b1165b26ee856da0c165fc33128660748c891f
guillaume lorand <guillaume.lorand@gmail.com>
2017-05-23 22:39:30 +0200

    dirty fix of midi keyboard listening

--- js/findchord.js
+++ js/findchord.js
@@ -3,6 +3,22 @@
 	var currentChord=[];
 	var pianoKeyboard ;
 	var synthe ;
+	var midiInput ;
+
+	const PIANOKEY_2_NOTE = {
+		"0":"c",
+		"1":"c#",
+		"2":"d",
+		"3":"d#",
+		"4":"e",
+		"5":"f",
+		"6":"f#",
+		"7":"g",
+		"8":"g#",
+		"9":"a",
+		"10":"a#",
+		"11":"b",
+	};
 	
 	function init()
 	{
@@ -28,6 +44,31 @@
 				check() ;
 			}
 		});
+		
+		midiInput = new MidiInput(function(press, pianoKey){
+			
+			console.log("pianoKey="+pianoKey);
+			var octave = Math.floor(pianoKey/12) ;
+			var key = pianoKey-(octave*12) ;
+			var note = PIANOKEY_2_NOTE[key] ;
+			
+			console.log("octave="+octave);
+			console.log("note="+note);
+			
+			if(press)
+			{
+				pianoKeyboard.lightUp(note+"/"+octave) ;
+				synthe.noteOn(note,octave) ;
+				check() ;
+			}
+			else
+			{
+				pianoKeyboard.turnOff(note+"/"+octave) ;
+				synthe.noteOff(note,octave) ;
+			}
+			
+		}) ;
+		
 		
 		addChord() ;
 	}
--- js/listenmidi.js
+++ js/listenmidi.js
@@ -1,54 +1,67 @@
 /**
  * 
  */
-var midiAccess = null; // the MIDIAccess object.
 
-window
-		.addEventListener(
-				'load',
-				function() {
-					if (navigator.requestMIDIAccess)
-						navigator.requestMIDIAccess().then(onMIDIInit,
-								onMIDIReject);
-					else
-						console.log("No MIDI support present in your browser.  You're gonna have a bad time.")
+var MidiInput = function(listener)
+{
+	var listener = listener ;
+	
+	var midiAccess = null; // the MIDIAccess object.
 
+	
+	function onMIDIInit(midi) {
+		midiAccess = midi;
 
-				});
-
-function onMIDIInit(midi) {
-	midiAccess = midi;
-
-	var haveAtLeastOneDevice = false;
-	var inputs = midiAccess.inputs.values();
-	for (var input = inputs.next(); input && !input.done; input = inputs.next()) {
-		input.value.onmidimessage = MIDIMessageEventHandler;
-		haveAtLeastOneDevice = true;
+		var haveAtLeastOneDevice = false;
+		var inputs = midiAccess.inputs.values();
+		for (var input = inputs.next(); input && !input.done; input = inputs.next()) {
+			input.value.onmidimessage = MIDIMessageEventHandler;
+			haveAtLeastOneDevice = true;
+		}
+		if (!haveAtLeastOneDevice)
+			console.log("No MIDI input devices present.  You're gonna have a bad time.");
 	}
-	if (!haveAtLeastOneDevice)
-		console.log("No MIDI input devices present.  You're gonna have a bad time.");
-}
 
-function onMIDIReject(err) {
-	console.log("The MIDI system failed to start.  You're gonna have a bad time.");
-}
+	function onMIDIReject(err) {
+		console.log("The MIDI system failed to start.  You're gonna have a bad time.");
+	}
 
-function MIDIMessageEventHandler(event) {
-	// Mask off the lower nibble (MIDI channel, which we don't care about)
-	switch (event.data[0] & 0xf0) {
-	case 0x90:
-		if (event.data[2] != 0) { // if velocity != 0, this is a note-on
-									// message
 
-			var value = event.data[1];
+	function MIDIMessageEventHandler(event) {
+		// Mask off the lower nibble (MIDI channel, which we don't care about)
+		switch (event.data[0] & 0xf0) {
+		case 0x90:
+			if (event.data[2] != 0) { // if velocity != 0, this is a note-on
+										// message
+				var value = event.data[1];
 
-			noteOn(event.data[1]);
+				listener(true, event.data[1]);
 
+				return;
+			}
+			// if velocity == 0, fall thru: it's a note-off. MIDI's weird, y'all.
+		case 0x80:
+			listener(false, event.data[1]);
 			return;
 		}
-		// if velocity == 0, fall thru: it's a note-off. MIDI's weird, y'all.
-	case 0x80:
-		noteOff(event.data[1]);
-		return;
 	}
+
+	
+	function oMidiInput(){
+		if (navigator.requestMIDIAccess)
+			navigator.requestMIDIAccess().then(onMIDIInit,
+					onMIDIReject);
+		else
+			console.log("No MIDI support present in your browser.  You're gonna have a bad time.") ;
+	}
+	
+	oMidiInput.prototype =
+	{
+		
+	}
+	
+	return new oMidiInput() ;
+	
+	
 }
+
